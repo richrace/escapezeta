a3a7cdfa0b2b08b4b478c93ed0fd62b3ef438201
Richard Race <race.richard@gmail.com>
2013-05-21 18:47:26 +0100

    Added simple Player object w/ Spec

---
+++ lib/player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_accessor :name
+
+  def initialize(player_name)
+    @name = player_name
+  end
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'player'
+
+describe Player do
+  
+  describe "#initialize" do
+
+    it "can give a player a name" do
+      test_string = "Rich"
+      player = Player.new test_string
+      player.name.should eq test_string
+    end
+    
+  end
+
+end
