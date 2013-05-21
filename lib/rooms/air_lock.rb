87ba67189604376095fdb81e705ccd599f57f121
Richard Race <race.richard@gmail.com>
2013-05-21 19:28:28 +0100

    Fixed requires; added strings for contents.

--- lib/rooms/air_lock.rb
+++ lib/rooms/air_lock.rb
@@ -1,13 +1,14 @@
-require 'rooms/room'
-require 'rooms/science_lab'
+require './lib/rooms/room'
+require './lib/rooms/science_lab'
 
 class AirLock < Room
 
   def initialize
-    super("Air Lock")
+    super("Air Lock" , ["Spacesuit", "Clipboard", "Flashlight"])
   end
 
   def move_up 
     ScienceLab.new
   end
+
 end
