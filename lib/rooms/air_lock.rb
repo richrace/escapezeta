b9c357518082d70aeb0b8365bedd5f966f2e2731
Richard Race <race.richard@gmail.com>
2013-05-22 12:20:42 +0100

    Added air lock open/closed boolean.

--- lib/rooms/air_lock.rb
+++ lib/rooms/air_lock.rb
@@ -2,8 +2,11 @@ require './lib/rooms/room'
 require './lib/rooms/science_lab'
 
 class AirLock < Room
+  attr_accessor :open
+  alias :open? :open
 
   def initialize
+    @open = false
     super("Air Lock" , ["Spacesuit", "Clipboard", "Flashlight"])
   end
 
