4c1cb9865a3794893f4baa92aa4a5ff856bbd578
Richard Race <race.richard@gmail.com>
2013-05-22 01:36:32 +0100

    Added move methods.

--- lib/rooms/science_lab.rb
+++ lib/rooms/science_lab.rb
@@ -1,9 +1,21 @@
-require 'rooms/room'
+require './lib/rooms/room'
 
 class ScienceLab < Room
   
   def initialize
     super("Science Lab")
+  end
+
+  def move_down
+    AirLock
+  end
+
+  def move_up
+    MessHall
+  end
+
+  def move_right
+    CrewQuarters
   end
 
 end
